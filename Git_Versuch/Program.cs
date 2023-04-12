8f0e30235caea8df860539cfb53605e905d0cfc2
Fabian Hammerschmidt <fabian.hammerschmidt@hs-augsburg.de>
2023-04-13 00:50:32 +0200

    Changes to the development branch without knowing about the feature

--- Git_Versuch/Program.cs
+++ Git_Versuch/Program.cs
@@ -10,7 +10,7 @@ namespace GitDemo
             Console.WriteLine("This is a simple C# program.");
             Console.WriteLine("It prints a message to the console.");
             Console.WriteLine("Git is awesome!");
-
+            Console.WriteLine("I have no idea of the new feature!");
         }
     }
 }
