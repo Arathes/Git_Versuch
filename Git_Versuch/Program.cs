16881b61c8d3f81883e57bbdd6aea9e3a23af3dd
Fabian Hammerschmidt <fabian.hammerschmidt@hs-augsburg.de>
2023-04-13 00:39:45 +0200

    Commit First Change

--- Git_Versuch/Program.cs
+++ Git_Versuch/Program.cs
@@ -9,6 +9,7 @@ namespace GitDemo
             Console.WriteLine("Hello Git!");
             Console.WriteLine("This is a simple C# program.");
             Console.WriteLine("It prints a message to the console.");
+            Console.WriteLine("Git is awesome!");
 
         }
     }
